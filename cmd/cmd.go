f237055d1f144abf900af61709800427ce5ada20
cs50-romain <cs50.git.romain@gmail.com>
2024-01-13 12:50:46 -0500

    Implemented showing and saving credentials

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,18 +1,34 @@
 package cmd
 
 import (
-	color "cs50-romain/gokey/pkg/colors"
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"syscall"
+
+	crypto "cs50-romain/gokey/internal/crypto"
+	color "cs50-romain/gokey/pkg/colors"
+
+	"golang.org/x/term"
 )
+
+type Creds struct {
+	Entries []struct {
+		Name              string    `json:"name"`
+		Username          string    `json:"username"`
+		EncryptedPassword string    `json:"encrypted_password"`
+	} `json:"entries"`
+}
 
 type Command struct {
 	Main	string
 	Name	string
 }
 
-func Execute(args []string) {
+func Execute(args []string, key []byte) {
 	if args[0] == "exit" {
 		os.Exit(0)
 	}
@@ -23,9 +39,13 @@ func Execute(args []string) {
 		log.Fatal("[ERROR] Invalid command;")
 	} else {
 		if command.Main == "new" {
+			storeCreds(args[2], key)
 			color.PrintBold("Creating new creds for " + command.Name, color.ElectricPink)
+			fmt.Println()
 		} else if command.Main == "show" {
 			color.PrintBold("Fetching creds for " + command.Name, color.ElectricPink)
+			fmt.Println()
+			showCreds(args[2], key)
 		} else {
 			color.PrintBold("Copying creds for " + command.Name, color.ElectricPink)
 		}
@@ -47,4 +67,74 @@ func buildCommand(args []string) *Command {
 		command.Name = args[2]
 	}
 	return &command
+}
+
+func showCreds(name string, key []byte) {
+	var creds Creds
+	b, err := os.ReadFile("gokey.json")
+	if err != nil {
+		log.Fatal("[ERROR] Error reading gokey.json; cmd.go -> ", err)
+	}
+
+	err = json.Unmarshal(b, &creds)
+	if err != nil {
+		log.Fatal("[ERROR] Error unmarshaling gokey.json; cmd.go -> ", err)
+	}
+
+	var password string
+	for _, entry := range creds.Entries {
+		if entry.Name == name {
+			fmt.Println(entry)
+			password = entry.EncryptedPassword	
+		}
+	}
+
+	b, err = crypto.Decrypt([]byte(password), key)
+	if err != nil {
+		log.Fatal("[ERROR] Error decrypting; cmd.go -> ", err)
+	}
+
+}
+
+func storeCreds(name string, key []byte) {
+	var creds Creds
+	b, err := os.ReadFile("gokey.json")
+	if err != nil {
+		log.Fatal("[ERROR] Error reading gokey.json; cmd.go -> ", err)
+	}
+	err = json.Unmarshal(b, &creds)
+	if err != nil {
+		log.Fatal("[ERROR] Error unmarshaling config.json; cmd.go -> ", err)
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	color.PrintBold("Enter username:", color.LimeGreen)
+	fmt.Println()
+	color.PrintBold(">", color.RoyalPurple)
+	username, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal("[ERROR] Error reading user input; cmd.go -> ", err)
+	}
+	username = strings.TrimSuffix(username, "\n")
+
+	color.PrintBold("Enter password:", color.LimeGreen)
+	fmt.Println()
+	color.PrintBold(">", color.RoyalPurple)
+	input, err := term.ReadPassword(int(syscall.Stdin)) 
+	if err != nil {
+		log.Fatal("[ERROR] Error reading user input; cmd.go -> ", err)
+	}
+		
+	encrypted_pass, err := crypto.Encrypt(input, key)
+	if err != nil {
+		log.Fatal("[ERROR] Error encrypting; cmd.go -> ", err)
+	}
+
+	creds.Entries = append(creds.Entries, struct{Name string "json:\"name\""; Username string "json:\"username\""; EncryptedPassword string "json:\"encrypted_password\""}{name, username, string(encrypted_pass)})
+
+	fmt.Println(creds.Entries)
+
+	b, err = json.Marshal(creds)
+
+	os.WriteFile("gokey.json", b, 444)
 }
