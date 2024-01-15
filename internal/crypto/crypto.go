2b67026a48c50f471b3d73918a1061c22c8d2eac
cs50-romain <cs50.git.romain@gmail.com>
2024-01-14 20:56:28 -0500

    Left log statement from debugging; Added random salt generator; Changed DeriveKey to use pbkdf2

--- internal/crypto/crypto.go
+++ internal/crypto/crypto.go
@@ -3,22 +3,17 @@ package crypto
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/sha256"
 	"crypto/rand"
+	"errors"
 	"io"
+	//"fmt"
+	"math/big"
+
+	"golang.org/x/crypto/pbkdf2"
 )
 
 func Encrypt(text, key []byte) ([]byte, error) {
-	desiredKeySize := 32
-	if len(key) < desiredKeySize {
-		// Pad the key with zeros or any other strategy.
-		for i := len(key); i < desiredKeySize; i++ {
-			key = append(key, 0)
-		}
-	} else if len(key) > desiredKeySize {
-		// Truncate the key.
-		key = key[:desiredKeySize]
-	}
-
 	c, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
@@ -34,21 +29,15 @@ func Encrypt(text, key []byte) ([]byte, error) {
 		return nil, err
 	}
 
-	return aesgcm.Seal(nonce, nonce, text, nil), nil
+	//fmt.Printf("Nonce: %x\n", nonce)
+	ciphertext := aesgcm.Seal(nil, nonce, text, nil)
+	result := append(nonce, ciphertext...)
+	//fmt.Printf("Result: %x\n", result)
+	return result, nil
 }
 
 func Decrypt(ciphertext, key []byte) ([]byte, error) {
-	desiredKeySize := 32
-	if len(key) < desiredKeySize {
-		// Pad the key with zeros or any other strategy.
-		for i := len(key); i < desiredKeySize; i++ {
-			key = append(key, 0)
-		}
-	} else if len(key) > desiredKeySize {
-		// Truncate the key.
-		key = key[:desiredKeySize]
-	}
-
+	//fmt.Printf("Encrypted Data: %x\n", ciphertext)
 	c, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
@@ -60,6 +49,30 @@ func Decrypt(ciphertext, key []byte) ([]byte, error) {
 	}
 
 	noncesize := aesgcm.NonceSize()
+	if len(ciphertext) < noncesize {
+		return nil, errors.New("ciphertext is too short to contain nonce")
+	}
+
 	nonce, ciphertext := ciphertext[:noncesize], ciphertext[noncesize:]
+	//fmt.Printf("Nonce: %x\n", nonce)
+	//fmt.Printf("Ciphertext: %x\n", ciphertext)
+
 	return aesgcm.Open(nil, nonce, ciphertext, nil)
+}
+
+func GenerateRandomSalt(length int) ([]byte, error) {
+    results := make([]byte, length)
+    for i := 0; i < length; i++ {
+        salt, err := rand.Int(rand.Reader, big.NewInt(255))
+        if err != nil {
+            return nil, err
+        }
+        results[i] = byte(salt.Int64())
+    }
+    return results, nil
+}
+
+func DeriveKey(masterPassword string, salt []byte) []byte {
+	// Use PBKDF2 to derive a fixed-length key
+	return pbkdf2.Key([]byte(masterPassword), salt, 100000, 32, sha256.New)
 }
