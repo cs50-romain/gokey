aeff84c6a03c1d4ee15c7b3e3541b7dab8a01a8d
cs50-romain <cs50.git.romain@gmail.com>
2024-01-15 19:06:24 -0500

    Added copy to clipboard feature

--- pkg/clipboard/clipboard.go
+++ pkg/clipboard/clipboard.go
@@ -1,1 +1,32 @@
 package clipboard
+
+import (
+	"log"
+	"os/exec"
+)
+
+func CopytoClipboard(output string) {
+	var copyCmd *exec.Cmd
+
+	copyCmd = exec.Command("xclip", "-selection", "C")
+
+	in, err := copyCmd.StdinPipe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := copyCmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := in.Write([]byte(output)); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := in.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	copyCmd.Wait()
+}
