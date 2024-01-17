e46472dad287b643613504d347cab02b3d473f51
cs50-romain <cs50.git.romain@gmail.com>
2024-01-16 21:22:24 -0500

    Fixed error handling:

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -11,8 +12,8 @@ import (
 	"syscall"
 
 	crypto "cs50-romain/gokey/internal/crypto"
-	color "cs50-romain/gokey/pkg/colors"
 	clipboard "cs50-romain/gokey/pkg/clipboard"
+	color "cs50-romain/gokey/pkg/colors"
 
 	"golang.org/x/term"
 )
@@ -35,86 +36,120 @@ func Execute(args []string, key []byte) {
 		os.Exit(0)
 	}
 
-	command := buildCommand(args)
+	command, err := buildCommand(args)
+	if err != nil {
+		log.Printf(err.Error())
+		return
+	}
 	
 	if command.Main != "new" && command.Main != "copy" && command.Main != "show" && command.Main != "list" {
-		log.Fatal("[ERROR] Invalid command;")
+		color.PrintBold("[ERROR] Invalid command;", color.Red)
+		fmt.Println()
 	} else {
 		if command.Main == "new" {
-			storeCreds(args[2], key)
+			err := storeCreds(args[2], key)
+			if err != nil {
+				log.Printf(err.Error())
+				return 
+			}
 			color.PrintBold("Creating new creds for " + command.Name, color.ElectricPink)
 			fmt.Println()
 		} else if command.Main == "show" {
 			color.PrintBold("Fetching creds for " + command.Name, color.ElectricPink)
 			fmt.Println()
 			showCreds(args[2], key)
+			if err != nil {
+				log.Printf(err.Error())
+				return 
+			}
 		} else if command.Main == "copy" {
 			color.PrintBold("Copying creds for " + command.Name, color.ElectricPink)
 			copyCreds(args[2], key)
+			if err != nil {
+				fmt.Println()
+				log.Printf(err.Error())
+				return 
+			}
 		} else {
 			color.PrintBold("List of saved credentials: ", color.ElectricPink)
 			fmt.Println()
 			listCreds()
+			if err != nil {
+				log.Printf(err.Error())
+				return 
+			}
 		}
 		fmt.Println()
 	}
 }
 
-func buildCommand(args []string) *Command {
+func buildCommand(args []string) (*Command, error) {
 	var command Command
 
 	if args[0] != "go" {
-		log.Fatal("[ERROR] Invalid command; go needs to be first argument")
+		return nil, errors.New("ERROR: Invalid command; go should be first argument")
 	}
 
 	if len(args) < 2 {
-		log.Fatal("[ERROR] Invalid command; Not enought arguments")
+		return nil, errors.New("ERROR: Not enough arguments")
 	} else {
 		command.Main = args[1]
-		if len(args) > 2 {
+		if command.Main == "new" || command.Main == "show" || command.Main == "copy" {
+			if len(args) < 3 {
+				return nil, errors.New("ERROR: Not enough arguments")
+			}
 			command.Name = args[2]
 		}
 	}
-	return &command
+	return &command, nil
 }
 
-func listCreds() {
+func listCreds() error {
 	var creds Creds
 	b, err := os.ReadFile("gokey.json")
 	if err != nil {
-		log.Fatal("[ERROR] Error reading gokey.json; cmd.go -> ", err)
+		return errors.New("[ERROR] Error reading gokey.json; cmd.go -> " + err.Error())
 	}
 
 	err = json.Unmarshal(b, &creds)
 	if err != nil {
-		log.Fatal("[ERROR] Error unmarshaling gokey.json; cmd.go -> ", err)
+		return errors.New("[ERROR] Error unmarshaling gokey.json; cmd.go -> " + err.Error())
 	}
 
 	for _, entry := range creds.Entries {
 		fmt.Println("Name: ", entry.Name)
 	}
+	return nil
 }
 
 func showCreds(name string, key []byte) {
-	username, password := getCreds(name, key)
+	username, password, err := getCreds(name, key)
+	if err != nil {
+		log.Printf(err.Error())
+		return
+	}
 	fmt.Printf("Username: %s\nPassword: %s\n", username, password)
 }
 
 func copyCreds(name string, key []byte) {
-	_, password := getCreds(name, key)	
+	_, password, err := getCreds(name, key)	
+	if err != nil {
+		log.Printf(err.Error())
+		return
+	}
 	clipboard.CopytoClipboard(password)
 }
 
-func getCreds(name string, key []byte) (string, string) {
+func getCreds(name string, key []byte) (string, string, error) {
 	var creds Creds
 	b, err := os.ReadFile("gokey.json")
 	if err != nil {
-		log.Fatal("[ERROR] Error reading gokey.json; cmd.go -> ", err)
+		return "", "", errors.New("[ERROR] Error reading gokey.json; cmd.go -> " + err.Error())
 	}
 
 	err = json.Unmarshal(b, &creds)
 	if err != nil {
-		log.Fatal("[ERROR] Error unmarshaling gokey.json; cmd.go -> ", err)
+		return "", "", errors.New("ERROR: could not unmarshal gokey.json; cmd.go -> " + err.Error())
 	}
 
 	var password string
@@ -128,26 +163,26 @@ func getCreds(name string, key []byte) (string, string) {
 
 	base64_password, err := base64.StdEncoding.DecodeString(password)
 	if err != nil {
-		log.Fatal("[ERROR] Error decoding base64 password; cmd.go -> ", err)
+		return "", "", errors.New("ERROR: could not decode base64 password; cmd.go -> " + err.Error())
 	}
 
 	b, err = crypto.Decrypt(base64_password, key)
 	if err != nil {
-		log.Fatal("[ERROR] Error decrypting; cmd.go -> ", err)
+		return "", "", errors.New("ERROR: Decryption issue; cmd.go -> " + err.Error())
 	}
 
-	return username, string(b)
+	return username, string(b), nil
 }
 
-func storeCreds(name string, key []byte) {
+func storeCreds(name string, key []byte) error {
 	var creds Creds
 	b, err := os.ReadFile("gokey.json")
 	if err != nil {
-		log.Fatal("[ERROR] Error reading gokey.json; cmd.go -> ", err)
+		return errors.New("ERROR: Error reading gokey.json; cmd.go -> " + err.Error())
 	}
 	err = json.Unmarshal(b, &creds)
 	if err != nil {
-		log.Fatal("[ERROR] Error unmarshaling config.json; cmd.go -> ", err)
+		return errors.New("ERROR: Error unmarshaling config.json; cmd.go -> " + err.Error())
 	}
 
 	reader := bufio.NewReader(os.Stdin)
@@ -156,7 +191,7 @@ func storeCreds(name string, key []byte) {
 	color.PrintBold(">", color.RoyalPurple)
 	username, err := reader.ReadString('\n')
 	if err != nil {
-		log.Fatal("[ERROR] Error reading user input; cmd.go -> ", err)
+		return errors.New("ERROR Error reading user input; cmd.go -> " + err.Error())
 	}
 	username = strings.TrimSuffix(username, "\n")
 
@@ -165,12 +200,12 @@ func storeCreds(name string, key []byte) {
 	color.PrintBold(">", color.RoyalPurple)
 	input, err := term.ReadPassword(int(syscall.Stdin)) 
 	if err != nil {
-		log.Fatal("[ERROR] Error reading user input; cmd.go -> ", err)
+		return errors.New("ERROR: Error reading user input; cmd.go -> " + err.Error())
 	}
 		
 	encrypted_pass, err := crypto.Encrypt(input, key)
 	if err != nil {
-		log.Fatal("[ERROR] Error encrypting; cmd.go -> ", err)
+		return errors.New("ERROR: Error encrypting; cmd.go -> " + err.Error())
 	}
 	fmt.Printf("Encrypted Pass: %x\n", encrypted_pass)
 
@@ -181,4 +216,5 @@ func storeCreds(name string, key []byte) {
 	b, err = json.Marshal(creds)
 
 	os.WriteFile("gokey.json", b, 444)
+	return nil
 }
